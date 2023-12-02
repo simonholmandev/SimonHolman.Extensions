e3f64e677ad2f0361b61fe381f2bcd7c5dafe06a
Simon Holman <65940542+simonholmantech@users.noreply.github.com>
2023-12-02 16:18:46 +1030

    Add project files.

---
+++ SimonHolman.Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SimonHolman.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool NotNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source != null && source.Any();
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source == null || !source.Any();
+        }
+    }
+}
---
+++ SimonHolman.Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SimonHolman.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool NotEmpty(this string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
